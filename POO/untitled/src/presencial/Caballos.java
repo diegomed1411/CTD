9624c189f1d5777f5e409b4542cea46278d8f137
Diego Medina <cr.diegomedina@gmail.com>
2023-07-24 20:37:56 -0300

    clase 01

---
+++ POO/untitled/src/presencial/Caballos.java
@@ -0,0 +1,16 @@
+package presencial;
+
+public class Caballos {
+    public static void main(String[] args){
+    String nombre = "Braulio";
+    String raza = "Criollo";
+    Integer edad = 2;
+    Double consumoSemanal = 1250.40;
+    String color = "Blanco";
+
+        System.out.println("El nombre del caballo es "+nombre);
+        System.out.println("La raza es "+raza);
+        System.out.println("su edad es "+edad+" y su color es "+color);
+        System.out.println("El consumo semanal es de: "+consumoSemanal);
+    }
+}
